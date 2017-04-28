513466bf0ca7eb9bb32b8a0ddba7f5fac6295c93
nestorcastro95 <nestorcastro@utexas.edu>
2017-04-27 19:09:48 -0500

    added parser helpers

--- ClingoParser.scala
+++ ClingoParser.scala
@@ -1,3 +1,41 @@
+import scala.language.implicitConversions
+
+
+
+object ClingoParser extends Regexparsers{
+	//whitespace info as demostrated by Loc's Chefparser
+	override def skipWhitespace = true;
+	override val whiteSpace = "[ \t\r\f]+".r
+
+	def clingoProgram: Parser[List[singleScript]] =
+		clingoScript ~ (newLine ~> clingoScript).* ^^ {
+			case r ~ l => r :: l 
+		}
+	def clingoScript: Parser[singleScript] =
+		factList.? ~ predicate.+ ^^ {
+			case None ~ _predicates =>
+				//have not implemented singleScript yet, will store
+				//a single set of facts and predicates
+				new singleScript(List(), _predicates)
+			case _facts ~ _predicates =>
+				new singleScript(_facts, _predicates)
+			//continue cases if necessary...
+		}
+	def factList: Parser[List[singleFacts]] =
+		fact.+
+	def fact: Parser[String] =
+		"""[A-Za-z]""".r ~ "(" ~ (number | word) ~ "," ~
+		(number | word) ~ ")." ^^ {
+		}
+
+
+
+
+
+}
+
+
+
 def predicate: Parser[String] =
 	"""[A-Za-z]""".r~"("~term~")" ^^ {
 		if(term)
